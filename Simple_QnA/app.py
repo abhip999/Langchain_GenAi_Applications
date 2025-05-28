35842fc2a0c3f3ae8be6f45c32b0d32d21313996
abhip999 <abhishekiitr999@gmail.com>
2025-05-28 15:13:51 +0530

    First commit

---
+++ Simple_QnA/app.py
@@ -0,0 +1,33 @@
+import os
+from dotenv import load_dotenv
+#from langchain_community.llms import Ollama
+from langchain_ollama import OllamaLLM
+import streamlit as st
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+#Langsmith Tracking
+os.environ['LANGCHAIN_API_KEY'] = os.getenv("LANGCHAIN_API_KEY")
+os.environ['LANGCHAIN_TRACING_V2'] = "true"
+os.environ['LANGCHAIN_PROJECT'] = os.getenv("LANGCHAIN_PROJECT")
+
+## Prompt template
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system","You are an AI Assistnat, please respond to the question asked"),
+        ("user","Question:{question}")
+    ]
+)
+## Streamlit framework
+st.title("Langchain demo with LLAMA3.2")
+input_text = st.text_input("What is in your mind?")
+
+##Ollama LLAMMA3.2 Model
+llm = OllamaLLM(model="llama3.2")
+output_parser = StrOutputParser()
+chain = prompt|llm|output_parser
+
+if input_text:
+    st.write(chain.invoke({"question":input_text}))
